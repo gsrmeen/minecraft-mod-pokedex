025eb2a4306a08cfe74dbdab945029ccf5fc6870
Patryk <patryk.gsrmeen@gmail.com>
2019-06-24 16:34:28 +0200

    Small update on command

--- src/main/java/com/gsrmeen/commands/PinfoCommand.java
+++ src/main/java/com/gsrmeen/commands/PinfoCommand.java
@@ -3,7 +3,9 @@ package com.gsrmeen.commands;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.server.command.ForgeCommand;
 
 
@@ -23,6 +25,15 @@ public class PinfoCommand extends ForgeCommand {
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        sender.sendMessage(new TextComponentString("Hello world :)"));
+        if (args.length != 1) {
+            String errorMessage = "Incorrect number of parameters, usage: /pinfo [pokemon]";
+            sender.sendMessage(new TextComponentString(errorMessage).setStyle(new Style().setColor(TextFormatting.RED)));
+            return;
+        }
+
+        String pokemonName = args[0];
+        String returnMessage = String.format("You are searching info for: %s", pokemonName);
+        sender.sendMessage(new TextComponentString(returnMessage));
+
     }
 }
